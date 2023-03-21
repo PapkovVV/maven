e2c11fcbe8931a3c35b6ef421ce466da962051e4
Valentine <getgggg2@gmail.com>
2023-03-21 12:56:23 +0300

    2

---
+++ src/main/java/ru/javacore/Main.java
@@ -0,0 +1,12 @@
+package ru.javacore;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		Tester tester = new Tester("Валентин", "Папков", 10, "B1", 500);
+		System.out.println(tester);
+		Tester.Strochka();
+	}
+
+}
